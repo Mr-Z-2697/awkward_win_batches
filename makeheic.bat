a4b768e4ace66cb96fe2e667cd50e8deb2a32d42
Mr-Z-2697 <74594146+Mr-Z-2697@users.noreply.github.com>
2020-11-20 23:13:43 -0600

    heic with alpha
    made failed attempt for 4 hours...
    then updating mp4box to nightly solves the problem.
    f*beep*k me.

--- makeheic.bat
+++ makeheic.bat
@@ -2,5 +2,17 @@
 set /p q=target quality? (crf): 
 set /p delsrc=delete source file? (confirm=y/else=n): 
 if not defined delsrc set delsrc=no
-for %%x in (%*) do ffmpeg -r 1 -i "%%~dpnxx"     -vf zscale=m=470bg:r=pc:f=5:d=error_diffusion,format=yuv444p10le     -frames 1 -c:v libx265 -preset 6 -crf %q%     -x265-params sao=0:selective-sao=0:ref=1:bframes=0:aq-mode=1:psy-rd=2:psy-rdoq=8:cbqpoffs=1:crqpoffs=1:range=full:info=0     "%temp%\%%~nxx.mp4"     && mp4box -add-image "%temp%\%%~nxx.mp4":primary -brand heic -new "%%~dpnx.heic"     && del "%temp%\%%~nxx.mp4"     && if %delsrc%==confirm del "%%~dpnxx" 
+for %%x in (%*) do (
+    ffmpeg -loglevel 0 -r 1 -i "%%~dpnxx" -vf zscale=m=470bg:r=pc:f=5:d=error_diffusion,format=yuv444p10le -frames 1 -f yuv4mpegpipe -strict -1 -|x265 --input - --y4m --preset 6 --crf %q% --no-sao --selective-sao 0 --ref 1 --bframes 0 --aq-mode 1 --psy-rd 2 --psy-rdoq 8 --cbqpoffs 1 --crqpoffs 1 --range full --colormatrix bt470bg --no-info -D 10 -o "%temp%\%%~nxx.heic.hevc"
+    ffmpeg -loglevel 0 -r 1 -i "%%~dpnxx" -vf extractplanes=a,zscale=r=pc,format=yuv444p10le -frames 1 -f yuv4mpegpipe -strict -1 -|x265 --input - --y4m --preset 6 --crf %q% --no-sao --selective-sao 0 --ref 1 --bframes 0 --aq-mode 1 --psy-rd 2 --psy-rdoq 8 --cbqpoffs 1 --crqpoffs 1 --range full --colormatrix bt470bg --no-info -D 10 -o "%temp%\%%~nxx.alpha.hevc"
+    if exist "%temp%\%%~nxx.alpha.hevc" (
+        mp4box -add-image "%temp%\%%~nxx.heic.hevc":primary -add-image "%temp%\%%~nxx.alpha.hevc":ref=auxl,1:alpha -brand heic -new "%%~dpnx.heic"
+        del "%temp%\%%~nxx.heic.hevc"
+        del "%temp%\%%~nxx.alpha.hevc"
+    )else (
+        mp4box -add-image "%temp%\%%~nxx.heic.hevc":primary -brand heic -new "%%~dpnx.heic"
+        del "%temp%\%%~nxx.heic.hevc"
+    )
+    if %delsrc%==confirm del "%%~dpnxx" 
+)
 pause
