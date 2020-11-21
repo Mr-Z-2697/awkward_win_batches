6ea0941427ef2415faa6273ec15c1c40f879e863
Mr-Z-2697 <74594146+Mr-Z-2697@users.noreply.github.com>
2020-11-21 02:27:41 -0600

    switch back to ffmpeg libx265
    for support of higher resolution

--- makeheic.bat
+++ makeheic.bat
@@ -3,16 +3,12 @@ set /p q=target quality? (crf):
 set /p delsrc=delete source file? (confirm=y/else=n): 
 if not defined delsrc set delsrc=no
 for %%x in (%*) do (
-    ffmpeg -loglevel 0 -r 1 -i "%%~dpnxx" -vf scale=out_color_matrix=470bg:out_range=pc:flags=lanczos:sws_dither=ed,format=yuv444p10le -frames 1 -f yuv4mpegpipe -strict -1 -|x265 --input - --y4m --preset 6 --crf %q% --no-sao --selective-sao 0 --ref 1 --bframes 0 --aq-mode 1 --psy-rd 2 --psy-rdoq 8 --cbqpoffs 1 --crqpoffs 1 --range full --colormatrix bt470bg --no-info -D 10 -o "%temp%\%%~nxx.heic.hevc"
-    ffmpeg -loglevel 0 -r 1 -i "%%~dpnxx" -vf extractplanes=a,scale=out_range=pc,format=yuv444p10le -frames 1 -f yuv4mpegpipe -strict -1 -|x265 --input - --y4m --preset 6 --crf %q% --no-sao --selective-sao 0 --ref 1 --bframes 0 --aq-mode 1 --psy-rd 2 --psy-rdoq 8 --cbqpoffs 1 --crqpoffs 1 --range full --colormatrix bt470bg --no-info -D 10 -o "%temp%\%%~nxx.alpha.hevc"
-    if exist "%temp%\%%~nxx.alpha.hevc" (
-        mp4box -add-image "%temp%\%%~nxx.heic.hevc":primary -add-image "%temp%\%%~nxx.alpha.hevc":ref=auxl,1:alpha -brand heic -new "%%~dpnx.heic"
-        del "%temp%\%%~nxx.heic.hevc"
-        del "%temp%\%%~nxx.alpha.hevc"
-    )else (
-        mp4box -add-image "%temp%\%%~nxx.heic.hevc":primary -brand heic -new "%%~dpnx.heic"
-        del "%temp%\%%~nxx.heic.hevc"
-    )
+    ffmpeg -hide_banner -r 1 -i "%%~dpnxx" -vf scale=out_color_matrix=470bg:out_range=pc:flags=lanczos:sws_dither=ed,format=yuv444p10le -frames 1 -c:v libx265 -preset 6 -crf %q% -x265-params no-sao=1:selective-sao=0:ref=1:bframes=0:aq-mode=1:psy-rd=2:psy-rdoq=8:cbqpoffs=1:crqpoffs=1:range=full:colormatrix=bt470bg:no-info=1:output-depth=10 "%temp%\%%~nxx.heic.hevc"
+    ffmpeg -hide_banner -r 1 -i "%%~dpnxx" -vf extractplanes=a,scale=out_range=pc,format=yuv444p10le -frames 1 -c:v libx265 -preset 6 -crf %q% -x265-params no-sao=1:selective-sao=0:ref=1:bframes=0:aq-mode=1:psy-rd=2:psy-rdoq=8:cbqpoffs=1:crqpoffs=1:range=full:colormatrix=bt470bg:no-info=1:output-depth=10 "%temp%\%%~nxx.alpha.hevc"
+    mp4box -add-image "%temp%\%%~nxx.heic.hevc":primary -brand heic -new "%%~dpnx.heic"
+    mp4box -add-image "%temp%\%%~nxx.alpha.hevc":ref=auxl,1:alpha -brand heic "%%~dpnx.heic"
+    del "%temp%\%%~nxx.heic.hevc"
+    del "%temp%\%%~nxx.alpha.hevc"
     if %delsrc%==confirm del "%%~dpnxx" 
 )
 pause
