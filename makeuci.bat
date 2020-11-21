b29b24a208416230256224462d329c79f9ac4f40
Mr-Z-2697 <74594146+Mr-Z-2697@users.noreply.github.com>
2020-11-21 01:33:58 -0600

    Add files via upload

--- makeuci.bat
+++ makeuci.bat
@@ -2,5 +2,8 @@
 set /p q=target quality? (crf): 
 set /p delsrc=delete source file? (confirm=y/else=n): 
 if not defined delsrc set delsrc=no
-for %%x in (%*) do cd /d "D:\Softwares\UCI\" & imgdec "%%~dpnxx" - | ucienc - -o "%%~dpnx.uci" -s -hevc -x "--crf %q% -p placebo --ref 1 --bframes 0 --aq-mode 1 --psy-rd 2 --psy-rdoq 8 --cbqpoffs -2 --crqpoffs -2 -F 1 --no-sao --output-depth 10" && if %delsrc%==confirm del "%%~dpnxx"
+for %%x in (%*) do (
+    cd /d "D:\Softwares\UCI\" & imgdec "%%~dpnxx" - | ucienc - -o "%%~dpnx.uci" -s -hevc -x "--crf %q% -p placebo --ref 1 --bframes 0 --aq-mode 1 --psy-rd 2 --psy-rdoq 8 --cbqpoffs 1 --crqpoffs 1 -F 1 --no-sao --output-depth 10"
+    if %delsrc%==confirm del "%%~dpnxx"
+)
 pause
