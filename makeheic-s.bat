e6e26b67d89e06893400355b4d13a983f56d0aeb
Mr-Z-2697 <74594146+Mr-Z-2697@users.noreply.github.com>
2021-01-25 01:19:36 +0800

    tag transfer func

--- makeheic-s.bat
+++ makeheic-s.bat
@@ -3,8 +3,8 @@ set /p q=target quality? (crf):
 set /p delsrc=delete source file? (confirm=y/else=n): 
 if not defined delsrc set delsrc=no
 for %%x in (%*) do (
-    ffmpeg -hide_banner -r 1 -i "%%~dpnxx" -vf scale=out_color_matrix=470bg:out_range=pc:flags=lanczos:sws_dither=ed,format=yuv444p10le -frames 1 -c:v libx265 -preset 6 -crf %q% -x265-params no-sao=1:selective-sao=0:ref=1:bframes=0:aq-mode=1:psy-rd=2:psy-rdoq=8:cbqpoffs=1:crqpoffs=1:range=full:colormatrix=bt470bg:no-info=1 "%temp%\%%~nxx.heic.hevc" -y
-    ffmpeg -hide_banner -r 1 -i "%%~dpnxx" -vf extractplanes=a,scale=out_range=pc,format=yuv444p10le -frames 1 -c:v libx265 -preset 6 -crf %q% -x265-params no-sao=1:selective-sao=0:ref=1:bframes=0:aq-mode=1:psy-rd=2:psy-rdoq=8:cbqpoffs=1:crqpoffs=1:range=full:colormatrix=bt470bg:no-info=1 "%temp%\%%~nxx.alpha.hevc" -y
+    ffmpeg -hide_banner -r 1 -i "%%~dpnxx" -vf scale=out_color_matrix=470bg:out_range=pc:flags=lanczos:sws_dither=ed,format=yuv444p10le -frames 1 -c:v libx265 -preset 6 -crf %q% -x265-params no-sao=1:selective-sao=0:ref=1:bframes=0:aq-mode=1:psy-rd=2:psy-rdoq=8:cbqpoffs=1:crqpoffs=1:range=full:colormatrix=bt470bg:transfer=iec61966-2-1:no-info=1 "%temp%\%%~nxx.heic.hevc" -y
+    ffmpeg -hide_banner -r 1 -i "%%~dpnxx" -vf extractplanes=a,scale=out_range=pc,format=yuv444p10le -frames 1 -c:v libx265 -preset 6 -crf %q% -x265-params no-sao=1:selective-sao=0:ref=1:bframes=0:aq-mode=1:psy-rd=2:psy-rdoq=8:cbqpoffs=1:crqpoffs=1:range=full:colormatrix=bt470bg:transfer=iec61966-2-1:no-info=1 "%temp%\%%~nxx.alpha.hevc" -y
     mp4box -add-image "%temp%\%%~nxx.heic.hevc":primary -brand heic -new "%%~dpnx.heic"
     mp4box -add-image "%temp%\%%~nxx.alpha.hevc":ref=auxl,1:alpha -brand heic "%%~dpnx.heic"
     del "%temp%\%%~nxx.heic.hevc"
